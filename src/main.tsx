05f9e56ef71393aed6ac422d47934e1e8884819e
AdriAir <aborio2004@gmail.com>
2024-11-16 22:07:46 +0100

    chore: scss removed

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {Home} from "./pages/home";
 import {Cpu} from "./pages/cpu";
 import {AppLayout} from "./pages";
-import './styles/main.scss';
 
 createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
