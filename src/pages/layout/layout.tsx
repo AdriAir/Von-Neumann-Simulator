9d7c69cf80fecebffd3527634b4d5f08cbb2d273
Antoniiocer <antoniocervantes.3005@gmail.com>
2024-11-16 23:54:52 +0100

    styles: changed styles

--- src/pages/layout/layout.tsx
+++ src/pages/layout/layout.tsx
@@ -1,13 +1,14 @@
 import {Outlet} from "react-router-dom";
 import WindowTravelling from "./windowTravelling/window-travelling.tsx";
-import DefaultButton from "../../components/buttons/default-button.tsx";
+import "./layout.scss"
 
 function AppLayout() {
 
 
     return <div>
-        <h3>Von Neumann Simulator</h3>
-        <DefaultButton text={'holaaa'}></DefaultButton>
+        <div className="layout">
+            <h3>Von Neumann Simulator</h3>
+        </div>
         <WindowTravelling/>
         <Outlet/>
     </div>
--- src/utils/constants/routes.ts
+++ src/utils/constants/routes.ts
@@ -1,5 +1,6 @@
 const __BASE_ROUTE = "/"
 export const APP_ROUTES = {
+    CPU: `${__BASE_ROUTE}cpu`,
     ALU: `${__BASE_ROUTE}arithmetic-unit`,
     MP: `${__BASE_ROUTE}memory-unit`,
     UC: `${__BASE_ROUTE}control-unit`,
